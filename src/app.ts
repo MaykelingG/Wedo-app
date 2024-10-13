8a84b1b0ecf24eb96ef44717471eb300e31f3566
RamonUlises <jorgitomilina@gmail.com>
2024-10-13 11:56:14 -0600

    Agregar autenticación básica para las rutas protegidas

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import chatbot from './router/chatbot';
 import usuarios from './router/usuarios';
 
 import './database/connection'
+import { authentication } from './Lib/Authentication';
 
 const app: express.Application = express();
 const port: number | string = process.env.PORT ?? 3000;
@@ -16,8 +17,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan('dev') as express.RequestHandler<StreamOptions>);
 
+app.use((req, res, next) => {
+  const response = authentication(req, res);
+  if(response) next();
+});
+
 app.use('/api/chatbot', chatbot);
-app.use('/api/usuarios', usuarios)
+app.use('/api/usuarios', usuarios);
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Ruta no encontrada' });
+});
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
