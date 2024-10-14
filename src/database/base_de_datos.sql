a5dc02445b64209dddf8f591c99cac2a76ae8a33
RamonUlises <jorgitomilina@gmail.com>
2024-10-14 14:19:05 -0600

    Actualizar base de datos y agregar foreign key para emprendimientos

--- src/database/base_de_datos.sql
+++ src/database/base_de_datos.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE proyecto_wedo;
 USE proyecto_wedo;
 
+-- EL UUID() GENERA UNA CADENA DE 36 CARACTERES QUE REPRESENTA UN IDENTIFICADOR ÚNICO UNIVERSAL
 CREATE TABLE usuarios(
   id_usuario CHAR(36) PRIMARY KEY NOT NULL DEFAULT (UUID()),
   nombres VARCHAR(80) NOT NULL,
@@ -14,7 +15,9 @@ CREATE TABLE emprendimiento(
   id_emprendimiento INT PRIMARY KEY AUTO_INCREMENT,
   nombre VARCHAR(80) NOT NULL,
   descripcion VARCHAR(80) NOT NULL,
-  financiamiento FLOAT NOT NULL
+  financiamiento FLOAT NOT NULL,
+  id_usuario CHAR(36),
+  FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario)
 );
 
 CREATE TABLE prestamo(
@@ -23,16 +26,6 @@ CREATE TABLE prestamo(
   interes FLOAT NOT NULL,
   plazo INT NOT NULL,
   frecuencia VARCHAR(10) NOT NULL
-);
-
-CREATE TABLE calculo (
-  id_calculo INT PRIMARY KEY AUTO_INCREMENT,
-  interes FLOAT,
-  cuotas INT,
-  total_cuotas INT,
-  pago_cuota FLOAT,
-  id_prestamo INT,
-  FOREIGN KEY (id_prestamo) REFERENCES prestamo (id_prestamo)
 );
 
 CREATE TABLE producto (
@@ -48,21 +41,14 @@ CREATE TABLE producto (
 
 CREATE TABLE segmentacion (
   id_segmentacion INT PRIMARY KEY AUTO_INCREMENT,
-  zona VARCHAR(10),
-  rango_edad VARCHAR(20),
-  densidad INT,
+  texto TEXT,
   id_producto INT,
-  id_departamento INT,
-  id_municipio INT,
-  id_oferta INT,
   FOREIGN KEY (id_producto) REFERENCES producto (id_producto)
 );
 
 CREATE TABLE potencial (
   id_potencial INT PRIMARY KEY AUTO_INCREMENT,
-  potencial_clientes FLOAT,
-  potencial_monetario FLOAT,
-  consumo_percapita FLOAT,
+  texto TEXT,
   id_producto INT,
   FOREIGN KEY (id_producto) REFERENCES producto (id_producto)
 );
@@ -73,8 +59,8 @@ CREATE TABLE inversion_activos (
   costo FLOAT,
   cantidad INT,
   utilidad INT,
+  clasificacion_activo VARCHAR(255),
   id_emprendimiento INT,
-  id_clasificacion_activo INT,
   FOREIGN KEY (id_emprendimiento) REFERENCES emprendimiento (id_emprendimiento)
 );
 
@@ -83,19 +69,6 @@ CREATE TABLE gastos_operacion (
   nombre VARCHAR(40),
   costo FLOAT,
   cantidad INT,
-  id_emprendimiento INT,
-  id_clasificacion_gasto INT,
-  FOREIGN KEY (id_emprendimiento) REFERENCES emprendimiento (id_emprendimiento)
-);
-
-CREATE TABLE inversion (
-  id_inversion INT PRIMARY KEY AUTO_INCREMENT,
-  total_inversion FLOAT,
-  total_depreciaciones FLOAT,
-  total_gasto FLOAT,
-  costo FLOAT,
-  inversion FLOAT,
-  capital FLOAT,
   id_emprendimiento INT,
   FOREIGN KEY (id_emprendimiento) REFERENCES emprendimiento (id_emprendimiento)
 );
@@ -106,15 +79,6 @@ CREATE TABLE material (
   medida VARCHAR(20),
   cantidad FLOAT,
   coste FLOAT,
-  id_producto INT,
-  FOREIGN KEY (id_producto) REFERENCES producto (id_producto)
-);
-
-CREATE TABLE margen (
-  id_margen INT PRIMARY KEY AUTO_INCREMENT,
-  costo FLOAT,
-  precio FLOAT,
-  margen FLOAT,
   id_producto INT,
   FOREIGN KEY (id_producto) REFERENCES producto (id_producto)
 );
@@ -142,17 +106,5 @@ BEGIN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'El formato del email no es válido';
     END IF;
-END; //
-DELIMITER ;
-
--- TRIGGER PARA ACTUALIZAR EL STOCK DE MATERIAL
-DELIMITER //
-CREATE TRIGGER actualizar_stock_material
-AFTER INSERT ON material
-FOR EACH ROW
-BEGIN
-    UPDATE producto
-    SET cantidad = cantidad - NEW.cantidad
-    WHERE id_producto = NEW.id_producto;
 END; //
 DELIMITER ;
